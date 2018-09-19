85460e72fb313ea0c8fbd67555a5150cd135e4f1
Mitchell Vandehaar <mitchvandehaar@gmail.com>
2018-09-19 11:30:11 -0700

    more temp fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,19 +9,18 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      test: {knownfor_img: [],
-        mentions: [],
-        knownfor_desc: [],
-      },
+      test: { "_id":"5ba1b12b0129ac786856e5de", "knownfor_img":[ "./assets/knownfor/takeout.svg", "./assets/knownfor/dinner.svg", "./assets/knownfor/dinner.svg", "./assets/knownfor/kids.svg" ], "knownfor_desc":[ "ipsam numquam velit", "quia ea doloremque", "et eaque est", "reprehenderit vel ipsam", "ut nostrum animi" ], "mentions":[ "https://s3.amazonaws.com/uifaces/faces/twitter/jjshaw14/128.jpg", "https://s3.amazonaws.com/uifaces/faces/twitter/superoutman/128.jpg", "https://s3.amazonaws.com/uifaces/faces/twitter/wintopia/128.jpg" ], "id":"5ba1b12b0129ac786856e57a", "name":"Rau, O'Conner and Franecki", "category":"Unbranded Frozen Pants", "desc":"dynamic whiteboard systems", "location":"North Romanstad", "expense":"$$", "food_rating":3.9, "decor_rating":5.4, "service_rating":3.1, "desc_bold":"Et quia rerum. Saepe neque optio quo maiores.", "__v":0 },
+      
       button: true,
       hideButton: true,
-      id: null,
+      id: '5ba1b12b0129ac786856e57a',
     };
     this.toggleButton = this.toggleButton.bind(this);
   }
 
   componentDidMount() {
-    this.getRestaurants();
+    // this.getRestaurants();
+    this.toggleButton()
   }
   
   getRestaurants() {
@@ -103,7 +102,7 @@ class App extends React.Component {
       </div>);
     } else {
       if (this.state.id && this.state.button === false) {
-        return (<div>
+        return (<div className="infoBody">
           <Info test={this.state.test} />
           <br></br><br></br><br></br>
           <KnownFor data={this.state.test} />
@@ -112,7 +111,7 @@ class App extends React.Component {
           </div>
         </div>);
       } else if (this.state.button === true) {
-        return (<div>
+        return (<div className="infoBody">
           <Info test={this.state.test} />
           <br></br><br></br><br></br>
           <KnownFor data={this.state.test} />
