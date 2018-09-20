dd9a930bb85f7269e82667b7a13243b5292bc345
Mitchell Vandehaar <mitchvandehaar@gmail.com>
2018-09-20 11:09:50 -0700

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,14 @@
-<<<<<<< HEAD
 import React from 'react';
 import $ from 'jquery';
 import Info from './components/Info.jsx';
 import KnownFor from './components/KnownFor.jsx';
 import styles from '../../public/info.css';
-=======
-import React from "react";
-import $ from "jquery";
-import Info from "./components/Info.jsx";
-import KnownFor from "./components/KnownFor.jsx";
-import styles from "../../public/info.css";
->>>>>>> c977f732951debf15e8c502f779d9a10d54f1f89
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       test: {
-<<<<<<< HEAD
         _id: '5ba1b12b0129ac786856e5de',
         knownfor_img: [
           './assets/knownfor/takeout.svg',
@@ -52,42 +43,6 @@ class App extends React.Component {
       button: true,
       hideButton: false,
       id: '5ba1b12b0129ac786856e5de'
-=======
-        _id: "5ba1b12b0129ac786856e5de",
-        knownfor_img: [
-          "./assets/knownfor/takeout.svg",
-          "./assets/knownfor/dinner.svg",
-          "./assets/knownfor/dinner.svg",
-          "./assets/knownfor/kids.svg"
-        ],
-        knownfor_desc: [
-          "ipsam numquam velit",
-          "quia ea doloremque",
-          "et eaque est",
-          "reprehenderit vel ipsam",
-          "ut nostrum animi"
-        ],
-        mentions: [
-          "https://s3.amazonaws.com/uifaces/faces/twitter/jjshaw14/128.jpg",
-          "https://s3.amazonaws.com/uifaces/faces/twitter/superoutman/128.jpg",
-          "https://s3.amazonaws.com/uifaces/faces/twitter/wintopia/128.jpg"
-        ],
-        id: "5ba1b12b0129ac786856e57a",
-        name: "Rau, O'Conner and Franecki",
-        category: "Unbranded Frozen Pants",
-        desc: "dynamic whiteboard systems",
-        location: "North Romanstad",
-        expense: "$$",
-        food_rating: 3.9,
-        decor_rating: 5.4,
-        service_rating: 3.1,
-        desc_bold: "Et quia rerum. Saepe neque optio quo maiores.",
-        __v: 0
-      },
-      button: true,
-      hideButton: true,
-      id: "5ba1b12b0129ac786856e5de"
-
     };
     this.toggleButton = this.toggleButton.bind(this);
   }
@@ -97,7 +52,6 @@ class App extends React.Component {
   }
 
   getRestaurants() {
-
     if (this.state.id === '5ba1b12b0129ac786856e5de') {
       this.toggleButton();
     } else {
@@ -130,35 +84,6 @@ class App extends React.Component {
         }
       });
     }
-
-    $.ajax({
-      url: "/restaurants/all",
-      type: "GET",
-      contentType: "application/json",
-      success: items => {
-        var randNum = Math.floor(Math.random() * items.length);
-        this.setState({
-          id: items[randNum].id
-        });
-        $.ajax({
-          url: `/restaurants/info/${this.state.id}`,
-          type: "GET",
-          contentType: "application/json",
-          success: items => {
-            this.setState({
-              test: items
-            });
-            this.toggleButton();
-          },
-          error: error => {
-            console.log(error);
-          }
-        });
-      },
-      error: error => {
-        console.log(error);
-      }
-    });
   }
   
 
@@ -185,7 +110,7 @@ class App extends React.Component {
         for (var i = 2; i < this.state.test.mentions.length; i++) {
           var mentionBox = document.getElementsByClassName(styles.knownDesc);
           if (mentionBox[i]) {
-            mentionBox[i].style.display = "none";
+            mentionBox[i].style.display = 'none';
           }
           this.setState({
             hideButton: false
@@ -197,7 +122,7 @@ class App extends React.Component {
         for (var i = 2; i < this.state.test.mentions.length; i++) {
           var mentionBox = document.getElementsByClassName(styles.knownDesc);
           if (mentionBox[i]) {
-            mentionBox[i].style.display = "inline-block";
+            mentionBox[i].style.display = 'inline-block';
           }
           this.setState({
             hideButton: false
@@ -233,11 +158,7 @@ class App extends React.Component {
             </div>
           </div>
         );
-<<<<<<< HEAD
       } else if (this.state.id && this.state.button === true) {
-=======
-      } else if (this.state.button === true) {
->>>>>>> c977f732951debf15e8c502f779d9a10d54f1f89
         return (
           <div className="infoBody">
             <Info test={this.state.test} />
